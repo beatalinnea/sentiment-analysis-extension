654f7cf3e67065b8d5f173fcdabde042aff542a4
Beata Eriksson <be222gr@student.lnu.se>
2025-02-22 15:34:38 +0100

    Use react chart.js

--- background.js
+++
@@ -1,3 +0,0 @@
-import Chart from './libs/Chart.min.js';
-
-window.Chart = Chart;
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ChartComponent from './ChartComponent'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React App with Chart.js</h1>
+      <ChartComponent />
+    </div>
+  )
+}
+
+export default App
