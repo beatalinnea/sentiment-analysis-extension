aa9c106df28e80c7755f9b3b2ecf35b43c82a9aa
Beata Eriksson <be222gr@student.lnu.se>
2025-04-08 14:23:21 +0200

    Open side panel on icon click

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+chrome.sidePanel
+          .setPanelBehavior({ openPanelOnActionClick: true })
+          .catch((error) => console.error(error));
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === "complete" || changeInfo.url) {
     updateUrl(tab.url)
